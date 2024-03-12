fbdc2646e437e9abff734659f6f3ee6880b01f7d
Hobbl <123955956+HobblHase@users.noreply.github.com>
2024-03-12 15:23:07 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-Config {}
+Config = {}
 
 Config.PutOnTool = 'screwdriverset' -- the item you want to put on the fakeplate IF YOU WANT NO ITEM TO DO THAT REPLACE Config.PutOnTool = 'screwdriverset' with Config.PutOnTool = nil
 Config.BreakChance = 20 -- chance 1-100 that the fakeplate breaks on remove
